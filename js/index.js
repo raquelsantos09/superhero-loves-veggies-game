0ecf316cdb993e18714b1c09f9a6e5ad501857d2
Raquel Santos <raquelmrsantos@gmail.com>
2023-01-26 14:16:40 +0000

    player and obstacles working

--- js/index.js
+++ js/index.js
@@ -60,11 +60,17 @@ const ctx = canvas.getContext('2d');
 let startDiv = document.getElementById("start");
 let gameOverScreen = document.getElementById("game-over");
 const startButton = document.querySelector("#start-button");
-const restartButton = document.querySelector("#restart-button");
+const restartButton = document.getElementById("restart-button");
 let livesDiv = document.getElementById("player-lives");
+let lives = document.querySelectorAll(".heart-img");
+let heart1 = document.getElementById("heart1");
+let heart2 = document.getElementById("heart2");
+let heart3 = document.getElementById("heart3");
 
-let myObstacles = [];
-let obsImages = [hamburguerImg, sodaImg, bananasImg, appleImg, orangeImg, grapesImg, watermelonImg, broccoliImg];
+let badObstacles = [];
+let goodObstacles = [];
+let obsBadImages = [hamburguerImg, sodaImg];
+let obsGoodImages = [bananasImg, appleImg, orangeImg, grapesImg, watermelonImg, broccoliImg];
 
 let superheroX = canvas.width/2-15;
 let superheroY = 380; 
@@ -82,70 +88,67 @@ let isMovingRight = false;
 let animateId = 0;
 let isGameOver = false;
 
+let playerLives = 3;
+
+
 
 window.addEventListener ('load', () => {
   canvas.style.display = 'none';
   startDiv.style.display = "block";
   gameOverScreen.style.display = "none";
   livesDiv.style.display = "none";
-      
-
+  
+  
   startButton.addEventListener('click', () => {
-      startGame();    
+    startGame();    
   })
 
-  restartButton.addEventListener('click', () => {
-    startGame();    
+    restartButton.addEventListener("click",() => {
+      window.location.reload()
+    })
+    
+    
+
+    // Moving the superhero:
+    document.addEventListener('keydown', event => {
+      console.log(event);
+      if (event.code == 'ArrowLeft') {
+        // move superhero to the left
+        isMovingLeft = true
+      }
+      if (event.code == 'ArrowRight') {
+        // move superhero to the right
+        isMovingRight = true
+      }
+    })
+    
+    
+    document.addEventListener('keyup', (event) => {
+      // making the superhero stop
+      isMovingLeft = false
+      isMovingRight = false
+    })
   })
   
 
-  // Moving the superhero:
-document.addEventListener('keydown', event => {
-  console.log(event);
-  if (event.code == 'ArrowLeft') {
-    // move superhero to the left
-    isMovingLeft = true
+ 
+ 
+ function startGame() {
+   startDiv.style.display = "none";
+   canvas.style.display = "block"; // Element is rendered as a block-level element
+   gameOverScreen.style.display = "none";
+   livesDiv.style.display = "flex";
+   animate();
   }
-  if (event.code == 'ArrowRight') {
-    // move superhero to the right
-    isMovingRight = true
-  }
-})
-
-
-document.addEventListener('keyup', (event) => {
-  // making the superhero stop
-  isMovingLeft = false
-  isMovingRight = false
-})
-})
-
-
-/*function initialScreen() {
-  startDiv.style.display = "block";
-  canvas.style.display = "none"; 
-  gameOverScreen.style.display = "none";
-  livesDiv.style.display = "none";
-  }
-  */
-
-
-function startGame() {
-    startDiv.style.display = "none";
-    canvas.style.display = "block"; // Element is rendered as a block-level element
-    gameOverScreen.style.display = "none";
-    livesDiv.style.display = "flex";
-    animate();
-}
-
-
-class Obstacle{
-  constructor(x,y,width,height){
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.image = obsImages[Math.floor(Math.random() * obsImages.length)];
+  
+  
+  class goodObstacle{
+    constructor(x,y,width,height){
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.image = obsGoodImages[Math.floor(Math.random() * obsGoodImages.length)];
   }
   drawObstacle() {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
@@ -156,74 +159,115 @@ class Obstacle{
       superheroX + superheroWidth > this.x &&
       superheroY < this.y + this.height &&
       superheroHeight + superheroY > this.y
-    ) {
-      isGameOver = true
+      ) {
+        isGameOver = false
+      }
     }
   }
-}
-
   
-function animate() {
+  class badObstacle{
+    constructor(x,y,width,height){
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.image = obsBadImages[Math.floor(Math.random() * obsBadImages.length)];
+  }
+  drawObstacle() {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+  checkCollision() {
+    if (
+      superheroX < this.x + this.width &&
+      superheroX + superheroWidth > this.x &&
+      superheroY < this.y + this.height &&
+      superheroHeight + superheroY > this.y
+      ) {
+        playerLives -= 1;
+        if(playerLives === 2) {
+          lives[0].src = "";
+        }
+      }
+    }
+  }
+  
+  function ckeckLives() {
+  if (playerLives <= 0) {
+      isGameOver = true;
+    }
+  }
+  
+  function drawScore() {
+    ctx.font = '18px serif';
+    ctx.fillStyle = 'black';
+  }
+
+
+  function animate() {
     ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
     ctx.drawImage(cloudsImg, 0, cloudsY, canvas.width, canvas.height);
     ctx.drawImage(superheroImg, superheroX, superheroY, superheroWidth, superheroHeight);
-   
+    
     if(animateId % 100 === 0) {
-      myObstacles.push (new Obstacle(Math.random() * canvas.width, -80, 80, 80));
+      goodObstacles.push (new goodObstacle(Math.random() * canvas.width, -80, 80, 80));
+      badObstacles.push (new badObstacle(Math.random() * canvas.width, -80, 80, 80));
     } 
-
-    myObstacles.forEach(obstacle => {
-      obstacle.drawObstacle();
-      obstacle.y += 2;
-      obstacle.checkCollision();
+    
+    goodObstacles.forEach(goodObstacle => {
+      goodObstacle.drawObstacle();
+      goodObstacle.y += 2;
+      goodObstacle.checkCollision();
     })      
 
-
- // score: function () {
-   // const points = Math.floor(this.frames / 5);
-    //this.context.font = '18px serif';
-    //this.context.fillStyle = 'black';
-   // this.context.fillText(`Score: ${points}`, 350, 50);
-  //}
-
-
-       
-  if (isMovingLeft === true) {
-    superheroX -= 6
-  }
-  if (isMovingRight === true) {
-    superheroX += 6
-  }
-  
-  if (superheroX < 0) {
-    superheroX = 0;
-  }
-  if (superheroX >= canvas.width - 50) {
-    superheroX = canvas.width - 50;
-  }
-
-
-
-         if(isGameOver === true) {
-          cancelAnimationFrame(animateId); 
-          gameOver();
-      } else {
-          animateId = requestAnimationFrame(animate);
+    badObstacles.forEach(badObstacle => {
+      badObstacle.drawObstacle();
+      badObstacle.y += 2;
+      badObstacle.checkCollision();
+    })  
+    
+    
+    // score: function () {
+      // const points = Math.floor(this.frames / 5);
+      //this.context.font = '18px serif';
+      //this.context.fillStyle = 'black';
+      // this.context.fillText(`Score: ${points}`, 350, 50);
+      //}
+      
+      
+      
+      if (isMovingLeft === true) {
+        superheroX -= 6
       }
-
+      if (isMovingRight === true) {
+        superheroX += 6
+      }
+      
+      if (superheroX < 0) {
+        superheroX = 0;
+      }
+      if (superheroX >= canvas.width - 50) {
+        superheroX = canvas.width - 50;
+      }
+      
+      
+      
+      if(isGameOver === true) {
+        cancelAnimationFrame(animateId); 
+        gameOver();
+      } else {
+        animateId = requestAnimationFrame(animate);
+      }
+      
       
     }
-  
-
-function gameOver() {
-    startDiv.style.display = "none";
-    canvas.style.display = "none";
-    gameOverScreen.style.display = "flex"; // Element is rendered as a block-level element
-    livesDiv.style.display = "none";
-
-
-
     
+    
+    function gameOver() {
+      startDiv.style.display = "none";
+      canvas.style.display = "none";
+      gameOverScreen.style.display = "flex"; // Element is rendered as a block-level element
+      livesDiv.style.display = "none";
+  
       }
 
   
